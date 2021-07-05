d514fd41de185dac8959194975a5c45e27c6c1aa
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2021-07-05 09:05:18 -0600

    Initial commit

---
+++ src/main/java/ste/projects/springsqlserver/SpringSqlServerApplication.java
@@ -0,0 +1,13 @@
+package ste.projects.springsqlserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSqlServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSqlServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ste/projects/springsqlserver/SpringSqlServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ste.projects.springsqlserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSqlServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
